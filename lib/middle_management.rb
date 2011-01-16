66122ccd86def1849ee603ea33fbd178fd7e65b1
Robby Grossman <robby@freerobby.com>
2011-01-16 12:38:04 -0500

    Provide Slacker class for production testing.

--- lib/middle_management.rb
+++ lib/middle_management.rb
@@ -1,3 +1,4 @@
 require 'middle_management/config'
 require 'middle_management/job_modifications'
 require 'middle_management/manager'
+require 'middle_management/slacker'
---
+++ lib/middle_management/slacker.rb
@@ -0,0 +1,7 @@
+module MiddleManagement
+  class Slacker
+    def self.slack_for(time_value_in_seconds)
+      sleep(time_value_in_seconds)
+    end
+  end
+end
