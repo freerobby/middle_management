0c3a08db0b47da42a267941fe805671bc6221e9e
Robby Grossman <robby@freerobby.com>
2011-01-16 01:20:45 -0500

    Calculate workers based on current runnable jobs rather than total jobs in queue.

--- lib/middle_management/job_modifications.rb
+++ lib/middle_management/job_modifications.rb
@@ -7,7 +7,7 @@ module Delayed
       class Job < ::ActiveRecord::Base
         def self.new_with_micromanagement
           self.new_without_micromanagement
-          MiddleManagement::Manager.enforce_number_of_current_jobs(self.count)
+          MiddleManagement::Manager.enforce_number_of_current_jobs(self.ready_to_run.count)
         end
         class << self
           alias_method :new_without_micromanagement, :new
@@ -16,7 +16,7 @@ module Delayed
         
         def destroy_with_micromanagement
           destroy_without_micromanagement
-          MiddleManagement::Manager.enforce_number_of_current_jobs(Delayed::Backend::ActiveRecord::Job.count)
+          MiddleManagement::Manager.enforce_number_of_current_jobs(Delayed::Backend::ActiveRecord::Job.ready_to_run.count)
         end
         alias_method :destroy_without_micromanagement, :destroy
         alias_method :destroy, :destroy_with_micromanagement
--- spec/lib/middle_management/job_modifications_spec.rb
+++ spec/lib/middle_management/job_modifications_spec.rb
@@ -9,8 +9,10 @@ describe Delayed::Backend::ActiveRecord::Job do
   describe "#new" do
     it "micromanages remaining jobs" do
       new_job = mock("New Job")
+      ready_to_run_jobs_mock = mock("Ready to Run")
       Delayed::Backend::ActiveRecord::Job.should_receive(:new_without_micromanagement).exactly(:once).and_return(new_job)
-      Delayed::Backend::ActiveRecord::Job.should_receive(:count).any_number_of_times.and_return(3)
+      Delayed::Backend::ActiveRecord::Job.should_receive(:ready_to_run).any_number_of_times.and_return(ready_to_run_jobs_mock)
+      ready_to_run_jobs_mock.should_receive(:count).any_number_of_times.and_return(3)
       MiddleManagement::Manager.should_receive(:enforce_number_of_current_jobs).with(3).exactly(:once)
       Delayed::Backend::ActiveRecord::Job.new
     end
@@ -26,7 +28,9 @@ describe Delayed::Backend::ActiveRecord::Job do
       job = Delayed::Backend::ActiveRecord::Job.new
       
       job.should_receive(:destroy_without_micromanagement).exactly(:once)
-      Delayed::Backend::ActiveRecord::Job.should_receive(:count).any_number_of_times.and_return(2)
+      ready_to_run_jobs_mock = mock("Ready to Run")
+      Delayed::Backend::ActiveRecord::Job.should_receive(:ready_to_run).any_number_of_times.and_return(ready_to_run_jobs_mock)
+      ready_to_run_jobs_mock.should_receive(:count).any_number_of_times.and_return(2)
       MiddleManagement::Manager.should_receive(:enforce_number_of_current_jobs).with(2).exactly(:once)
       job.destroy
     end
